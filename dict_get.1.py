d9ce1e5080d41d38b5ae547857efd949acd94715
carello <cp2carello@gmail.com>
2018-02-22 12:06:10 -0600

    dict get use case 	new file:   dict_get.1.py

---
+++ dict_get.1.py
@@ -0,0 +1,9 @@
+
+mydict = {'Name': 'Zabra', 'Age': 7}
+print("Value : {}".format(mydict.get('Age')))
+
+# If key doesn't exist, use a default. ie "Never"
+print("Value : {}".format(mydict.get('Education', "Never")))
+
+
+print('Value : {}'.format(mydict['Age']))
