24dc9c3a429270f9fe2cebad7e96b960e2441b24
carello <cp2carello@gmail.com>
2018-10-06 20:59:47 -0500

    Example logging template - simple!

---
+++ logging_template.py
@@ -0,0 +1,16 @@
+
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+'''
+Add the "logging.debug" statement as needed per example below
+'''
+
+if data[0] == 'say':
+    logging.debug('Incoming message: {}'.format(data[0]))    # just add a statement like this.
+    sc_conn.sendall(data[0].encode('utf-8'))
