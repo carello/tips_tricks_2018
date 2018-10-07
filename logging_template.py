733c6225fb41dfff957d5307fbfc556f1ccfdf22
carello <cp2carello@gmail.com>
2018-10-06 21:31:29 -0500

    Example logging template - Added ability to write to a file and examples

--- logging_template.py
+++ logging_template.py
@@ -1,6 +1,9 @@
 
 import logging
 
+##########################################
+# Example 1
+
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.DEBUG,
@@ -16,3 +19,46 @@ Add the "logging.debug" statement as needed per example below
 if data[0] == 'say':
     logging.debug('Incoming message: {}'.format(data[0]))    # just add a statement like this.
     sc_conn.sendall(data[0].encode('utf-8'))
+
+
+
+###################################
+# Example 2
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    #filename='tcp_server.log',
+    #filemode='w',
+    handlers=[
+        logging.FileHandler("tcp_server.log"),
+        logging.StreamHandler(sys.stdout)
+    ])
+# logging.getLogger().addHandler(logging.StreamHandler())
+
+
+#############################
+# Example 3
+# using __name__ best convention
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S',
+    #filename='tcp_server.log',
+    #filemode='w',
+    handlers=[
+        logging.FileHandler("tcp_server.log"),
+        logging.StreamHandler(sys.stdout)
+    ])
+# logging.getLogger().addHandler(logging.StreamHandler())
+
+
+if data[0] == 'say':
+    logger.debug('Incoming message: {}'.format(data[0]))
+    sc_conn.sendall(data[0].encode('utf-8'))
+
+# ===========================
